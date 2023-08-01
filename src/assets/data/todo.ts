524254c14cdb8d943a211f7e4d65c873778ca537
gustavofcosta <gfcdesenvolvimento@gmail.com>
2023-08-01 17:23:10 -0300

    alterando data

--- src/assets/data/todo.ts
+++ src/assets/data/todo.ts
@@ -7,11 +7,10 @@ export const todo: ITodo[] = [
   {
     title: 'Lorem Ipsum',
     description:
-      'is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only ',
+      'is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown ',
   },
   {
     title: 'Lorem Ipsum',
-    description:
-      'is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only ',
+    description: 'is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys ',
   },
 ];
